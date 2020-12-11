0bdb6d2dc53db5b0e35547625083f0f98c48e3a4
g8kig <paul.winwood@sky.com>
2020-12-11 19:05:18 +0000

    Added tasks.json note

--- SOFTWARE/DMBasic/src/Maximite-Olimex.X/build.cmd
+++ SOFTWARE/DMBasic/src/Maximite-Olimex.X/build.cmd
@@ -5,3 +5,21 @@ make -f Makefile.mak build -j2
 cd Z:\Users\paul\
 
 
+rem tasks.json
+rem {
+rem     // See https://go.microsoft.com/fwlink/?LinkId=733558
+rem     // for the documentation about the tasks.json format
+rem     "version": "2.0.0",
+rem     "tasks": [
+rem         {
+rem             "label": "Build",
+rem             "type": "process",
+rem             "command": "/Applications/Wine Devel.app/Contents/Resources/wine/bin/wine",
+rem             "args": ["start", "/b", "/wait", "z:/users/paul/build.cmd"],
+rem             "group": {
+rem                 "kind": "build",
+rem                 "isDefault": true
+rem             }
+rem         }
+rem     ]
+rem }
